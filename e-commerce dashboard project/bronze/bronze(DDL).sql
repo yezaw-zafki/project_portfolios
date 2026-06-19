4fa913b2d1d6d9b93a86aeab220d33063c965c99
Ye Zaw Myint <71198367+yezaw-zafki@users.noreply.github.com>
2026-06-19 09:04:47 +0700

    Fix formatting and spacing in source table DDL

--- e-commerce dashboard project/bronze/bronze(DDL).sql
+++ e-commerce dashboard project/bronze/bronze(DDL).sql
@@ -3,18 +3,18 @@
 -- ***************************
 USE bronze;
 CREATE TABLE source (
-orderid VARCHAR(50),
-date DATE,
-customerid VARCHAR(50),
-product VARCHAR(50),
-quantity INT,
-unitprice DECIMAL(10,2),
-shpping_address VARCHAR(50),
-paymeny_method VARCHAR(50),
-orderstatus VARCHAR(50),
-tracking_number VARCHAR(50),
-items_in_cart INT,
-coupon_code VARCHAR(50),
-referral_source VARCHAR(50),
-total_price DECIMAL(20,2)
+    orderid VARCHAR(50),
+    date DATE,
+    customerid VARCHAR(50),
+    product VARCHAR(50),
+    quantity INT,
+    unitprice DECIMAL(10 , 2 ),
+    shpping_address VARCHAR(50),
+    paymeny_method VARCHAR(50),
+    orderstatus VARCHAR(50),
+    tracking_number VARCHAR(50),
+    items_in_cart INT,
+    coupon_code VARCHAR(50),
+    referral_source VARCHAR(50),
+    total_price DECIMAL(20 , 2 )
 );
